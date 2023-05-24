35204692a27a2edb90d777ab44cf6633da9c83dd
Rishi95555 <127435110+Rishi95555@users.noreply.github.com>
2023-05-23 19:49:30 -0700

    comment

--- Ball.java
+++ Ball.java
@@ -1,40 +1,44 @@
 import java.awt.*;
 import java.util.*;
 
-public class Ball extends Rectangle{
+public class Ball extends Rectangle {
 
 	Random random;
-	int xVelocity;
+	int velX;
 	int yVelocity;
-	int initialSpeed = 2;
-	
-	Ball(int x, int y, int width, int height){
-		super(x,y,width,height);
+	int initialSpeed = 10;
+
+	Ball(int x, int y, int width, int height) {
+		super(x, y, width, height);
 		random = new Random();
 		int randomXDirection = random.nextInt(2);
-		if(randomXDirection == 0)
+		if (randomXDirection == 0)
 			randomXDirection--;
-		setXDirection(randomXDirection*initialSpeed);
-		
+		setXDirection(randomXDirection * initialSpeed);
+
 		int randomYDirection = random.nextInt(2);
-		if(randomYDirection == 0)
+		if (randomYDirection == 0)
 			randomYDirection--;
-		setYDirection(randomYDirection*initialSpeed);
-		
+		setYDirection(randomYDirection * initialSpeed);
+
 	}
-	
+
 	public void setXDirection(int randomXDirection) {
-		xVelocity = randomXDirection;
+		velX = randomXDirection;
 	}
+
 	public void setYDirection(int randomYDirection) {
 		yVelocity = randomYDirection;
 	}
+
 	public void move() {
-		x += xVelocity;
+		x += velX;
 		y += yVelocity;
 	}
+
 	public void draw(Graphics g) {
-		g.setColor(Color.blue);
+		g.setColor(Color.white);
 		g.fillOval(x, y, height, width);
 	}
+
 }
--- GameFrame.java
+++ GameFrame.java
@@ -1,16 +1,16 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class GameFrame extends JFrame{
+public class GameFrame extends JFrame {
 
 	GamePanel panel;
-	
-	GameFrame(){
+
+	GameFrame() {
 		panel = new GamePanel();
 		this.add(panel);
 		this.setTitle("PIG POG");
 		this.setResizable(false);
-		this.setBackground(Color.magenta);
+		this.setBackground(Color.DARK_GRAY);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.pack();
 		this.setVisible(true);
